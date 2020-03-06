f4a000f6f7b7130002349a8466c01012285c9d1b
bartek <chladek13@gmail.com>
2020-03-06 15:38:29 +0100

    Initial commit. Animated car, stars and planet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
+import { ReactComponent as Scene } from './scene.svg';
+import { gsap, MotionPathPlugin } from 'gsap/all';
 
 function App() {
+  const wrapper = useRef(null);
+
+  useEffect(() => {
+    const [elements] = wrapper.current.children;
+    const planet = elements.getElementById('planet');
+    const stars = elements.getElementById('stars');
+    const car = elements.getElementById('car');
+
+    gsap.registerPlugin(MotionPathPlugin);
+
+    gsap.set([planet, ...stars.children, car], { autoAlpha: 0 });
+
+    const t1 = gsap.timeline({ defaults: { ease: 'power3.inOut' } });
+
+    t1.to([planet, car], { duration: 2, autoAlpha: 1 })
+      .to(stars.children, {
+        duration: 1.5,
+        autoAlpha: 1,
+        stagger: 0.12
+      })
+      .to(car, {
+        duration: 1,
+        y: '+=20',
+        x: '+=55',
+        ease: 'Power1.easeIn'
+      })
+      .to(car, {
+        duration: 1.2,
+        scale: 0,
+        motionPath: {
+          path: '#CarPath',
+          align: '#CarPath',
+          autoRotate: 160,
+          alignOrigin: [0.5, 0.5]
+        }
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={wrapper} className="App">
+      <Scene />
     </div>
   );
 }
